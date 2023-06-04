be2bb9ae7e2686adef53d0ceb627c65034494447
Jflores-2019424 <107268421+Jflores-2019424@users.noreply.github.com>
2023-06-03 22:01:13 -0600

    Banco

---
+++ Banco/BancoReact/src/bancos/pages/BancoMenu.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const BancoMenu = () => {
+  return (
+    <div>
+      <h1>Menu</h1>
+      <hr />
+      <h2>Transacciones</h2>
+  
+    </div>
+  )
+}
---
+++ Banco/BancoReact/src/bancos/pages/BancoProfile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const BancoProfile = () => {
+  return (
+    <div>
+        <h1>Perfil</h1>
+        <hr />
+        <p>Datos</p>
+    </div>
+  )
+}

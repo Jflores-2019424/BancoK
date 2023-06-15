f8e2740506906c030f92c241e1c92ba40797736c
Jflores-2019424 <107268421+Jflores-2019424@users.noreply.github.com>
2023-06-15 15:51:42 -0600

    Add files via upload

---
+++ Banco/BancoReact/Banco/BancoReact/src/bancos/pages/BancoMenu.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const BancoMenu = () => {
+  return (
+    <div className='container border border-dark mt-5 divi '>
+      <h1>Menu</h1>
+      <hr />
+      <p>Transacciones</p>
+      <div className="container bg-dark translate">
+        <h1>55</h1>
+      </div>
+      <div className="container bg-dark translate">
+        <h1>4135</h1>
+      </div>
+      <div className="container bg-dark translate">
+      <h1>654</h1>
+      </div>
+    </div>
+  )
+}

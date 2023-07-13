8e0420ac6e4538e4f158bde16b6416f77dae24af
HNavarro2017175 <109392569+HNavarro2017175@users.noreply.github.com>
2023-07-13 09:26:05 -0600

    Se cambiaron nuevas cosas en el Backend

---
+++ BancoK-hnavarro/Bank-Project/index.js
@@ -0,0 +1,17 @@
+"use strict"
+
+const express = require("express");
+const app = express();
+const { connection } = require("./src/database/connection");
+require("dotenv").config();
+const port = process.env.PORT;
+const routes = require ("./src/routes/user.routes");
+
+connection();
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use("/api", routes);
+app.listen(port, function(){
+    console.log(`El servidor esta corriendo en el puerto: ${port}`);
+})

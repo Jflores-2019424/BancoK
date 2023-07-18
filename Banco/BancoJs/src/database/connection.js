67d0fe7cd02ea6a27616a6c9f0074dd136f60b0f
Jflores-2019424 <jflores-2019424@kinal.edu.gt>
2023-07-18 11:56:58 -0600

    Backend

--- Banco/BancoJs/src/database/connection.js
+++ Banco/BancoJs/src/database/connection.js
@@ -1,14 +1,14 @@
 'use strict'
 
 require('dotenv').config();
-const database = process.env.DATABASE;
+const database = process.env.URI_MONGO;
 const mongoose = require("mongoose");
 mongoose.set('strictQuery', true);
 
 const connection = async() => {
     try{
         await mongoose.connect(database);
-        console.log("Se estableció la conexion con la Base de Datos!");
+        console.log("Se conecto con MongoDB");
     }catch(error){
         throw new Error(error)
     }       
